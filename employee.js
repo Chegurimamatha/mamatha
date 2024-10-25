3a314da0e420ef15d1ea67639a3e0e90e6a5aa69
mamatha <chegurimamatha51@gmail.com>
2024-10-25 11:59:09 +0530

    projet code added

---
+++ employee.js
@@ -0,0 +1,11 @@
+class employee{
+    constructor(){
+        this.id=1001;
+        this.name="MAMMU"
+    }
+    display(){
+         console.log(this.id+" "+this.name)
+    }
+}
+let e=new employee();
+e.display();
---
+++ m2.js
@@ -0,0 +1,8 @@
+const sayHello=()=>{
+    console.log("this is sayHello function")
+}
+const sayHi=()=>{
+    console.log("this is arrow function")
+}
+sayHello();
+sayHi();
